2f576380846c9d865119529dce5959e66027ee3a
wjwesley <93011574+wjwesley@users.noreply.github.com>
2025-07-02 12:04:24 -0700

    Add files via upload
    added notes to PolyTests

--- PolyTests.cpp
+++ PolyTests.cpp
@@ -20,7 +20,7 @@ void removeLeadingZeros(vector<int>& v){
 
 class IntPoly{
     private: 
-    vector<int> coeffs; //written in reverse order: {3,4,5} is 3+4x+5x^2
+    vector<int> coeffs; //written in reverse order: {3,4,5} is 3+4a+5a^2
 
     public: 
     IntPoly(vector<int> v = {})
@@ -72,6 +72,11 @@ class IntPoly{
         removeLeadingZeros(v); 
         return IntPoly(v); 
     }
+
+    /*
+        {1,3} = 1 + 3x
+        {1,3,0} = 1 + 3x 
+    */
     
     IntPoly operator+(IntPoly const& obj)
     {
@@ -156,10 +161,10 @@ class IntPoly{
         reverse(q.begin(),q.end());
         return IntPoly(q);
     }
-
     };
 
-bool properBound(IntPoly p, IntPoly l, IntPoly u, int aLower, int aUpper){
+bool properBound(IntPoly p, IntPoly l, IntPoly u, int aLower, int aUpper){ 
+    //returns true if and only if  l(a) <= p(a) <= u(a) for all a with aLower <= a <= aUpper 
     for (int a = aLower; a <= aUpper; a++){
         int pa = p.eval(a); 
         int la = l.eval(a); 
@@ -171,7 +176,7 @@ bool properBound(IntPoly p, IntPoly l, IntPoly u, int aLower, int aUpper){
     return true;  
 }
 
-vector<IntPoly> bruteForceCubic(vector<int> L, vector<int> U){
+vector<IntPoly> bruteForceCubic(vector<int> L, vector<int> U){ //generates all integer coefficient cubic polynomials where the coefficient of x^i is bounded by L[i] and U[i] 
     vector<IntPoly> S;
     vector<int> v = {0,0,0,0};
     for (int i0 = L[0]; i0 <= U[0]; i0++){
@@ -186,7 +191,7 @@ vector<IntPoly> bruteForceCubic(vector<int> L, vector<int> U){
                     vector<int> w = {0};
                     IntPoly q(w); 
                     IntPoly r = q+p; 
-                    r.print();
+                    //r.print();
                     S.push_back(r);
                 }
             }
@@ -200,10 +205,53 @@ int main(){
     IntPoly g({4,5,2});
     f.divideByFactor(g).print();
 
-//Generation tests
+//Upper bound: goal is to show that R_3(a(x-y) = (a+1)z) <= a^3+5a^2+8a for a>=7
+//Generation tests: generate a set U of polynomials that
 
+//*are bounded between 1 and the conjectured Rado number a^3 + 5a^2 + 8a for all a >= 7
+//*do this by calling properBound
+
+//Problem: How to generate U? 
+/*Naive way: brute force: generate ALL polynomials with coefficients bounded by some constant, e.g. 
+    1,2,3,4,a,2a,3a,4a,a+1,2a+1,3a+1,4a+1,a+2,2a+2,... 
+
+    can do this with bruteForceCubic method
+
+    problem: get too many that aren't "useful" i.e. they don't appear in solutions to a(x-y) = (a+1)z 
+*/
+
+/*Alternative: generate many polynomials that appear in solutions together. 
+
+    if we have a set S of polynomials, then take all possible pairs (x,y) and look at the LHS of the equation
+
+    a(x-y) is another polynomial. 
+    
+    If this is divisible by a+1, then we can divide by a+1 and get a new polynomial z which we can add to our set S. 
+
+    Example: If x = 2a^2 + 2, y = 2a^2 - a + 1, then a(x-y) = a(a+1), so then z = a gives a solution, and we can add the polynomial z = a to S
+
+    iterate some number of times to get more solutions
+*/
+
+/*Experiments: 
+
+brute force with L = {-5,-5,-5,0}, U = {5,5,5,1} gave some conflicts during SAT solve 
+alternative method starting with smaller brute force set (L = {0,-1,-3,0}, U = {0,1,3,1}) gave zero conflicts (easy) 
+alternative method starting with "relevant" polynomials (multiples of a^2 {0,0,1,0}, {0,0,2,0},{0,0,3,0},..., multiples of a, a^3+5a^2+8a, etc.) also gave zero conflicts 
+
+Want to find sets with many conflicts in the SAT solver output (harder to solve) 
+The Rado number itself should always be in the set (otherwise instance is necessarily satisfiable) 
+
+*/
+
+//*enumerate the solutions to a(x-y) = (a+1)z where x,y,z are polynomials (in the variable a) in the set U 
+//*want UNSATISFIABLE cnf file 
+
+
+    //brute force a set of polynomials S 
     vector<IntPoly> S = bruteForceCubic({0,-1,-3,0},{0,1,3,1});
 
+    //throw out polynomials in S that are not bounded properly 
     vector<IntPoly> T; 
     for (IntPoly p : S){ 
          if (properBound(p,IntPoly({1,0,0,0}),IntPoly({0,8,5,1}),10,100)){
@@ -211,21 +259,24 @@ int main(){
          }
     }
 
+    //conjectured rado number upper bound polynomial 
     vector<int> radoUB = {0,8,5,1};
+
+    //some polynomials that appear in the proof 
     vector<vector<int>> relevant = {{0,8,5,1},{-1,8,5,1},{-2,8,5,1},{-3,8,5,1},{0,7,5,1},{0,6,5,1},{0,5,5,1},{0,8,4,1},{0,0,1,0},{0,0,2,0},{0,0,3,0},{0,0,4,0},{0,1,0,0},{0,2,0,0}
 ,{0,3,0,0},{0,0,1,1},{0,0,2,1},{0,0,3,1},{1,3,3,1},{1,2,3,1}};
 
-    T.push_back(IntPoly(radoUB));
-
-    cout << S.size() << endl;  
-    cout << T.size() << endl; 
-
-    vector<IntPoly> U = T; 
     for (auto p : relevant){
         if (properBound(p,IntPoly({1,0,0,0}),IntPoly({0,8,5,1}),10,100))
         U.push_back(IntPoly(p));
     }
     
+    
+    
+
+
+    /* "alternative method" iteratively generate more polynomials that appear in solutions and add to the set U 
+    */
     
     int iter = 0;
     int maxIter = 2;
@@ -241,7 +292,7 @@ int main(){
                 IntPoly LHS = (x-y).multiplyX();
                 if (LHS.isRoot(-1)){
                     IntPoly z = LHS.divideByFactor(IntPoly({1,1}));
-                    if (properBound(z,IntPoly({1,0,0,0}),IntPoly({0,8,5,1}),10,100)){
+                    if (properBound(z,IntPoly({1,0,0,0}),IntPoly({0,8,5,1}),10,100)){ //whenever we add z to the set, need to check that it's bounded correctly 
                         newU.push_back(z);
                     }
                 }
@@ -253,6 +304,15 @@ int main(){
         iter++;
     }
 
+     //test different possibilities for the set U
+
+
+    /* should make separate method
+    Given a set U, generate the CNF file for the SAT solver 
+
+    Currently a bit inefficient (should be able to rewrite so that it runs in quadratic rather than cubic time)
+    Should instead iterate over all pairs (x,y), then check if divisible by a+1, then check if a(x-y)/(a+1) belongs to the set U 
+    */
 
     ofstream cnffile("redDiagTest2.cnf"); 
     cnffile << "p cnf " << U.size()*3 << " " << 1 << endl;  
@@ -277,6 +337,8 @@ int main(){
             }
         }
     }
+
+
     cnffile.close(); 
     return 0; 
 }
